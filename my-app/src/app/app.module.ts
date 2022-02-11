65d194e728045c7915e613fe41c91d1f508f6f12
Samuele Minissale <minissalesamuele@gmail.com>
2022-02-11 15:36:26 +0100

    struttura completata

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -2,10 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { UserContainerComponent } from './project/user-container/user-container.component';
+import { UserListComponent } from './project/user-list/user-list.component';
+import { UserSingleComponent } from './project/user-single/user-single.component';
+import { UserDetailComponent } from './project/user-detail/user-detail.component';
+import { OrderContainerComponent } from './project/order-container/order-container.component';
+import { OrderListComponent } from './project/order-list/order-list.component';
+import { OrderSingleComponent } from './project/order-single/order-single.component';
+import { OrderDetailComponent } from './project/order-detail/order-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserContainerComponent,
+    UserListComponent,
+    UserSingleComponent,
+    UserDetailComponent,
+    OrderContainerComponent,
+    OrderListComponent,
+    OrderSingleComponent,
+    OrderDetailComponent
   ],
   imports: [
     BrowserModule
