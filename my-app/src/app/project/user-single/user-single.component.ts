561c4d71e96024ebad89a17b9c1dcbe2e0f36978
Samuele Minissale <minissalesamuele@gmail.com>
2022-02-13 16:29:02 +0100

    esercizio4 completed

--- my-app/src/app/project/user-single/user-single.component.ts
+++ my-app/src/app/project/user-single/user-single.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { User } from 'src/app/models/User';
 
 @Component({
   selector: 'app-user-single',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-single.component.css']
 })
 export class UserSingleComponent implements OnInit {
+
+  @Input() user!: User;
 
   constructor() { }
 
